f019082d810ed7297b49b80ebd9e614856d164e9
Dmitry Kozhedubov <hiisi13@gmail.com>
2021-12-02 12:54:45 +0300

    Installed JAICP Chat Widget

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+        <script src="https://bot.jaicp.com/chatwidget/vpwJUxmo%3A91951e0ea07876566641d521a5f3314c6b16739b/justwidget.js" async></script>
       </Head>
 
       <main>
